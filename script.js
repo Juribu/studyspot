20da35629ababc445d12d86ee8e63fcc99eadc0d
Jay Zheng <jz2384@cornell.edu>
2025-06-16 20:17:15 -0400

    set up layout

--- script.js
+++
@@ -1,9 +0,0 @@
-const start = document.getElementById("Start");
-const stop = document.getElementById("Stop");
-const pomodoro = document.getElementById("Pomodoro")
-const shortBreak = document.getElementById("Short")
-const longBreak = document.getElementById("Long")
-
-let timeLeft = 1500;
-let interval;
-
